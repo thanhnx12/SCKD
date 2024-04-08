bf8c32af269c5e4ab0213cc61019795e6fb0fbea
thanhnx12 <thanhnx1209@gmail.com>
2024-04-08 19:22:32 +0700

    nothing

--- main-mlm.py
+++ main-mlm.py
@@ -215,6 +215,9 @@ def train_mem_model(config, encoder, dropout_layer, classifier, training_data, e
             infoNCE_loss = 0
             for i in range(output.shape[0]):
                 neg_prototypes = [prototype[rel_id] for rel_id in prototype.keys() if rel_id != origin_labels[i].item()]
+                # neg_samples = [new_relation_data[rel_id] for rel_id in new_relation_data.keys() if rel_id != origin_labels[i].item()]
+                # neg_prototypes.extend(neg_samples)
+
                 neg_prototypes = torch.stack(neg_prototypes).to(config.device)
                 
                 #--- prepare batch of negative samples 
